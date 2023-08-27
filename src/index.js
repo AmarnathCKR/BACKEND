6445cd9c00537664da4763a8f9bc6d448e9f99cf
AmarnathCKR <ckramarnath@gmail.com>
2023-08-28 00:16:28 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://192.168.123.222:5173"],
+        origin: ["http://localhost:5173", "http://192.168.123.222:5173", "https://main--radiant-nasturtium-e14351.netlify.app"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
     })
